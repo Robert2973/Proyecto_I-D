864b9cc8d3b9ae1188d9b8d356697b77f189f3ec
Proyecto_I+D <roberto.hernandezap@alumno.buap.mx>
2025-10-13 22:45:10 -0600

    Prototipo 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,5 +72,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.0.1")
 
     implementation("androidx.viewpager2:viewpager2:1.1.0-beta01")
+    implementation("com.google.android.material:material:1.6.1")
 
 }
